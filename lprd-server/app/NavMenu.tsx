92268119a79b5a789394d3cff8cc0b60f814d60f
mwegmann <mario.wegmann@hs-augsburg.de>
2024-05-31 12:01:17 +0200

    added simple navbar

---
+++ lprd-server/app/NavMenu.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+import styles from './NavMenu.module.css';
+
+export default function NavMenu() {
+    return (
+        <nav className={styles.nav}>
+            <ul className={styles.links}>
+                <li>
+                    <Link href={'/displays'}>Displays</Link>
+                </li>
+                <li>
+                    <Link href={'/assets'}>Assets</Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
